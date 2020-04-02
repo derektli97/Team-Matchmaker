5ebecc72fa0b5372d5030516aba9afef505ff212
Ethan Partin <espartin98@gmail.com>
2020-04-01 22:59:18 -0500

    Add topics tags for students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,11 @@
 class Student < ApplicationRecord
   belongs_to :section
   belongs_to :project
+
+  @all_project_tags = ['AI', 'ML', 'Networking', 'Operating Systems', 'Cloud Computing', 'Cyber Security', 'Data Science',
+                         'Web Development', 'Graphics', 'Robotics', 'Algorithm Theory']
+
+  def self.all_project_tags
+      return @all_project_tags
+  end
 end
